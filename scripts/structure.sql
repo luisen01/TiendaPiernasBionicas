79d0466fc678795391e04075955bd6f0c1396f35
luisen01 <91671504+luisen01@users.noreply.github.com>
2022-03-15 12:56:57 -0700

    Add files via upload

---
+++ scripts/structure.sql
@@ -0,0 +1,17 @@
+USE iaw_db;
+DROP table if exists productos;
+CREATE TABLE productos (
+        codigo INT AUTO_INCREMENT PRIMARY KEY,
+        descripcion VARCHAR(200),
+        porcentajeRebaja INT,
+        estaRebajado ENUM('Si','No'),
+        precio DECIMAL (6,2)
+        );
+DROP table if exists clientes;
+CREATE TABLE clientes (
+        dni VARCHAR(9) PRIMARY KEY,
+        nombre VARCHAR(100),
+        apellidos VARCHAR(200),
+        fecha_nac DATE,
+        telefono INT
+        );
