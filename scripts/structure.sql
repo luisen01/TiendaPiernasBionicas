ead88bfe67baa21ac94b9ea8d1225d2216226ad7
luisen01 <91671504+luisen01@users.noreply.github.com>
2022-03-16 01:53:57 +0100

    Update structure.sql

--- scripts/structure.sql
+++ scripts/structure.sql
@@ -3,15 +3,22 @@ DROP table if exists productos;
 CREATE TABLE productos (
         codigo INT AUTO_INCREMENT PRIMARY KEY,
         descripcion VARCHAR(200),
-        porcentajeRebaja INT,
-        estaRebajado ENUM('Si','No'),
-        precio DECIMAL (6,2)
+        rebaja INT,
+        estaRebajas int NOT NULL,
+        precio int NOT NULL
         );
 DROP table if exists clientes;
 CREATE TABLE clientes (
-        dni VARCHAR(9) PRIMARY KEY,
-        nombre VARCHAR(100),
-        apellidos VARCHAR(200),
-        fecha_nac DATE,
-        telefono INT
+        nombre varchar(20) NOT NULL,
+        pApellido varchar(20) NOT NULL,
+        sApellido varchar(20) NOT NULL,
+        DNI VARCHAR(9) PRIMARY KEY
+        );
+DROP table if exists proveedores;
+CREATE TABLE proveedores (
+        nombre varchar(20) NOT NULL,
+        cif varchar(9) NOT NULL PRIMARY KEY,
+        direccion varchar(20) NOT NULL,
+        email varchar(20) NOT NULL,
+        telefono VARCHAR(9) PRIMARY KEY
         );
